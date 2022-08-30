ddf583272d1979789c0cd80b436c60178d021121
Jiyong Choi <jiyochoi@c7r4s5.42seoul.kr>
2022-08-30 12:04:04 +0900

    C01

---
+++ C01/ex07/ft_rev_int_tab.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jiyochoi <jiyochoi@student.42seoul.kr      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/29 12:00:58 by jiyochoi          #+#    #+#             */
+/*   Updated: 2022/08/30 10:00:04 by jiyochoi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int	i;
+	int	j;
+	int	tmp;
+
+	i = 0;
+	while (i < size / 2)
+	{
+		j = size - 1 - i;
+		tmp = tab[i];
+		tab[i] = tab[j];
+		tab[j] = tmp;
+		i++;
+	}
+}
---
+++ C01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jiyochoi <jiyochoi@student.42seoul.kr      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/29 12:11:39 by jiyochoi          #+#    #+#             */
+/*   Updated: 2022/08/30 09:58:31 by jiyochoi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	i;
+	int	j;
+	int	tmp;
+
+	i = 0;
+	while (i < size - 1)
+	{	
+		j = 0;
+		while (j < size - 1 - i)
+		{
+			while (tab[j] > tab[j + 1])
+			{
+				tmp = tab[j];
+				tab[j] = tab[j + 1];
+				tab[j + 1] = tmp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
